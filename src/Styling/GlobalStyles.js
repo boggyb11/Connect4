5292b72f58ca4cf6c0186370fd2cd2c3026e18f1
ben <benjamin_fitzpatrick@allfleetservices.co.uk>
2020-04-09 17:04:16 +0100

    that'll do donkey, that'll do

--- src/Styling/GlobalStyles.js
+++
@@ -1,8 +0,0 @@
-// styles/global.js
-import css from 'styled-jsx/css'
-
-export default css.global`
-  .game-board {
-    background: rgb(69, 69, 69);
-    width: 500px;
-`
--- src/components/grid.component.js
+++ src/components/grid.component.js
@@ -1,26 +1,13 @@
-
 import React from "react";
-// import globalStyles from "../Styling/GlobalStyles";
-// import _JSXStyle from "styled-jsx/style";
 
 export const Grid = () => {
     return (
         <>
-            {/* <h2>Connect 4 Grid</h2>
+            <h2>Connect 4 Grid</h2>
             <div className="GameBoard">
-
+ss
             </div>
-            <style jsx global>{globalStyles}</style> */}
-            <p>only this paragraph will get the style :)</p>
 
-            { /* you can include <Component />s here that include
-     other <p>s that don't get unexpected styles! */ }
-
-            <style jsx>{`
-  p {
-    color: red;
-  }
-`}</style>
         </>
     );
 }
